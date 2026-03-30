65019d1f2094002d40671a1ec386c44bb8470ae2
Wout Van Eynde <wout.vaneynde@kuleuven.be>
2026-03-30 14:38:50 +0200

    docs: add precaution warning AI methods

--- pages/LOSDT.py
+++ pages/LOSDT.py
@@ -560,7 +560,7 @@ def display_results(session_folder: Path, smiles_string: str, has_pdb: bool):
         radial_dir = session_folder / Config.RADIAL_PLOTS_DIR
 
         if radial_dir.exists():
-            st.warning("""Be cautious of ADMET-AI predictions, they are not always accurate and should be used as a guide rather than absolute truth.""")
+            st.warning("""Be cautious of ADMET-AI and Dimorphite-DL predictions, they are not always accurate and should be used as a guide rather than absolute truth.""")
             plot_files = sorted(list(radial_dir.glob("*.png")))
 
             if plot_files:
@@ -1036,7 +1036,7 @@ def display_results(session_folder: Path, smiles_string: str, has_pdb: bool):
 
             with tab4:
                 if complexes_dir.exists():
-                    complex_files = sorted(list(complexes_dir.glob("*complex.cif")))
+                    complex_files = sorted(list(complexes_dir.glob("*complex.pdb")))
 
                     if complex_files:
                         # Display current structure
